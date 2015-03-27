9884afef1a74746ef1ffac6f88da5ad46a8ee8cc
Cesar Tapasco <cethapgames@gmail.com>
2015-03-26 23:36:17 -0500

    Archivos de ejemplo cali.js

---
+++ appOS.nw/main.js
@@ -0,0 +1,38 @@
+var gui = require('nw.gui');
+var win = gui.Window.get();
+
+function m (){
+	win.minimize();
+}
+
+function mx (){
+	win.maximize();
+}
+
+function c (){
+	document.getElementsByClassName("mac")[0].style.display = "none";
+	document.getElementsByClassName("windows")[0].style.display = "none";
+	//loadScene();
+	timer();
+	setTimeout(function(){
+		timer();
+		setTimeout(function(){
+			timer();
+			setTimeout(function(){
+				win.close();
+			}, 1000);			
+		}, 200);		
+	}, 200);
+}
+
+function chge(){
+	var ms = document.getElementsByClassName("mac")[0].style.display;
+	if(ms == "block" || ms == ""){
+		document.getElementsByClassName("mac")[0].style.display = "none";
+		document.getElementsByClassName("windows")[0].style.display = "block";
+	}else{
+		document.getElementsByClassName("windows")[0].style.display = "none";
+		document.getElementsByClassName("mac")[0].style.display = "block";
+	}
+
+}
